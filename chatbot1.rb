37cff359d1cf8fe54129c0fe597d7888724f5ef2
India Dearlove <india_dearlove@yahoo.co.uk>
2014-12-08 13:45:29 +0000

    Add colour to the chatbot

--- chatbot1.rb
+++ chatbot1.rb
@@ -1,3 +1,5 @@
+require './colour.rb'
+
 def get_response(input)
   key = RESPONSES.keys.select {|k| /#{k}/ =~ input }.sample
   /#{key}/ =~ input
@@ -12,21 +14,21 @@ def get_response(input)
   response.nil? ? 'sorry?' : response % { c1: $1, c2: $2}
 end
 
-RESPONSES = { 'goodbye' => 'bye', 
-              'sayonara' => 'sayonara', 
-              'the weather is (.*)' => 'I hate it when it\'s %{c1}', 
-              'I love (.*)' => 'I love %{c1} too', 
-              'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}',
-              'I hate (.*)' =>'What\'s wrong with %{c1}?',
-              'Let\'s go to (.*)' =>'I\'m a computer, I can\'t go to %{c1}',
-              'I am (.*)' =>'Nice to meet you %{c1}, I am the chatbot',
-              'Hello' => 'Hi',
-              'Who are you?' => 'The chatbot'
+RESPONSES = { 'goodbye' => 'bye'.red, 
+              'sayonara' => 'sayonara'.red, 
+              'the weather is (.*)' => 'I hate it when it\'s %{c1}'.red, 
+              'I love (.*)' => 'I love %{c1} too'.red, 
+              'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}'.red,
+              'I hate (.*)' =>'What\'s wrong with %{c1}?'.red,
+              'Let\'s go to (.*)' =>'I\'m a computer, I can\'t go to %{c1}'.red,
+              'I am (.*)' =>'Nice to meet you %{c1}, I am the chatbot'.red,
+              'Hello' => 'Hi'.red,
+              'Who are you?' => 'The chatbot'.red   
             }
 
 puts "Hello, what's your name?"
 name = gets.chomp
-puts "Hello #{name}"
+puts "Hello #{name}".red
 while(input = gets.chomp) do
   puts get_response(input)
 end
