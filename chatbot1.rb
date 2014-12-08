d3bf8287daae4dc9378a02382b81a3fabd025cbd
India Dearlove <india_dearlove@yahoo.co.uk>
2014-12-08 11:39:40 +0000

    Added responses 1

--- chatbot1.rb
+++ chatbot1.rb
@@ -12,8 +12,10 @@ RESPONSES = { 'goodbye' => 'bye',
               'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}',
               'I hate (.*)' =>'What\'s wrong with %{c1}?',
               'Let\'s go to (.*)' =>'I\'m a computer, I can\'t go to %{c1}'
-              
-              }
+              'I am (.*)' =>'Nice to meet you %{c1}, I am the chatbot'
+              'Hello' => 'Hi'
+              'Who are you?' => 'The chatbot'
+            }
 
 puts "Hello, what's your name?"
 name = gets.chomp
