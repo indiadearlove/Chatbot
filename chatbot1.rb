2ee8d8c9817348a0335df978c8c9bff155e0aa16
India Dearlove <india_dearlove@yahoo.co.uk>
2014-12-08 11:46:46 +0000

    Added responses 1 edit

--- chatbot1.rb
+++ chatbot1.rb
@@ -2,6 +2,13 @@ def get_response(input)
   key = RESPONSES.keys.select {|k| /#{k}/ =~ input }.sample
   /#{key}/ =~ input
   response = RESPONSES[key]
+  
+=begin
+  
+  if response = 'quit'
+
+=end  
+
   response.nil? ? 'sorry?' : response % { c1: $1, c2: $2}
 end
 
@@ -11,9 +18,9 @@ RESPONSES = { 'goodbye' => 'bye',
               'I love (.*)' => 'I love %{c1} too', 
               'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}',
               'I hate (.*)' =>'What\'s wrong with %{c1}?',
-              'Let\'s go to (.*)' =>'I\'m a computer, I can\'t go to %{c1}'
-              'I am (.*)' =>'Nice to meet you %{c1}, I am the chatbot'
-              'Hello' => 'Hi'
+              'Let\'s go to (.*)' =>'I\'m a computer, I can\'t go to %{c1}',
+              'I am (.*)' =>'Nice to meet you %{c1}, I am the chatbot',
+              'Hello' => 'Hi',
               'Who are you?' => 'The chatbot'
             }
 
